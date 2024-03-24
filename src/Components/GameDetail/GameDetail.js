73ee28374a1e7ca5a4109022aeae74d38a7821e1
Hussnain Raza <hussnain444all@gmail.com>
2024-03-24 09:36:22 +0500

    managed project assets

--- src/Components/GameDetail/GameDetail.js
+++ src/Components/GameDetail/GameDetail.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './GameDetail.css'
 import { Rating } from '@mui/material'
-import play from './play.png'
+import play from '../../assets/images/play.png'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import IconButton from '@mui/material/IconButton';
